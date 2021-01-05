b4ccbfa79698893b6b2a8f1491bd7aaf66919064
JTuyuc7 <50525507+JTuyuc7@users.noreply.github.com>
2021-01-05 01:36:12 -0600

    Hotel gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,59 @@
+import React from "react"
+import { css } from '@emotion/core';
+import styled from '@emotion/styled';
+import Layout from "../components/Layout";
+import ImagenHotel from '../components/ImagenHotel';
+import ContenidoInicio from '../components/contenidoInicio';
+import useHabitaciones from '../hooks/use-Habitaciones';
+import HabitacionPreview from '../components/HabitacionPreview';
+
+
+const ListadoHabitaciones = styled.ul`
+  max-width: 1200px;
+  width: 95%;
+  margin: 4rem auto 0 auto;
+
+  @media (min-width: 768px){
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    column-gap: 3rem;
+
+  }
+`;
+
+const IndexPage = () => {
+
+  const habitaciones = useHabitaciones();
+
+  //console.log(habitaciones);
+
+  return(
+ 
+    <Layout>
+      <ImagenHotel />
+
+      <ContenidoInicio />
+
+      <h2 css={css`
+        text-align: center;
+        margin-top: 5rem;
+        font-size: 4rem;
+      `}
+
+      >Nuestras Habitaciones</h2>
+
+      <ListadoHabitaciones>
+        {habitaciones.map( habitacion => (
+          <HabitacionPreview 
+            key={habitacion.id}
+            habitacion={habitacion}
+          />
+        ))}
+      </ListadoHabitaciones>
+
+    </Layout>
+      
+  )
+}
+
+export default IndexPage
